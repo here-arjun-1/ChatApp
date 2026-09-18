54f3eec33ecc2e4b7905b333d06fa9ad8ea83ca8
Arjun Vyas <arjunvyas2006@gmail.com>
2026-09-19 00:48:29 +0530

    feat: add chat models

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,8 @@
-package server
+package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("websockets")
+	r := gin.Default()
+	r.Run(":9090")
 }
---
+++ internal/models/message.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Message struct {
+	UUID       string    `json:"uuid"`
+	SenderID   string    `json:"sender_id"`
+	ReceiverID string    `json:"receiver_id"`
+	Content    string    `json:"content"`
+	CreatedAt  time.Time `json:"created_at"`
+}
