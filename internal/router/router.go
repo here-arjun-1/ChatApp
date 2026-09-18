457c8e6e2984db4fd6c2cebd3374a6a62eb917a5
Arjun Vyas <arjunvyas2006@gmail.com>
2026-09-19 01:06:21 +0530

    feat: add health router

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,1 +1,16 @@
 package router
+<<<<<<< HEAD
+=======
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func New() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/health", handlers.Health)
+
+	return r
+}
+>>>>>>> f0c79c5 (feat: add health router)
