abf54de05f638a8576bd718c4cf8b71b1e5f2f91
Arjun Vyas <arjunvyas2006@gmail.com>
2026-09-19 11:57:07 +0530

    refactor: simplify Message model for 2-person chat

--- internal/models/message.go
+++ internal/models/message.go
@@ -3,9 +3,7 @@ package models
 import "time"
 
 type Message struct {
-	UUID       string    `json:"uuid"`
-	SenderID   string    `json:"sender_id"`
-	ReceiverID string    `json:"receiver_id"`
-	Content    string    `json:"content"`
-	CreatedAt  time.Time `json:"created_at"`
+	From      string    `json:"from"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
 }
--- internal/models/user.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type User struct {
-	UUID string `json:"uuid"`
-	Name string `json:"name"`
-}
